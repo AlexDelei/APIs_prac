7777d1a4d5823cb4cce088a9cc4f35c00ee7d8a0
AlexDelei <alexdelei125@gmail.com>
2024-04-04 16:26:52 +0300

    url network

---
+++ DjangoRestFramework/tutorial/snippets/urls.py
@@ -0,0 +1,11 @@
+"""
+Wire the views up into these urls
+"""
+from django.urls import path
+from snippets import views
+
+
+urlpatterns = [
+    path('snippets/', views.snippet_list),
+    path('snippets/<int:pk>/', views.snippet_detail)
+]
